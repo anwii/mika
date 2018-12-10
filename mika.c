9370297a816ab2f8240503391ea239bf75cfb405
Jonah Mann <jonah.d.mann@gmail.com>
2018-12-10 10:06:09 +0800

    Add files via upload
    Added 'rock-paper-scissors' and 'repeat what I say'.

--- mika.c
+++ mika.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+
+void rps(char* userInput);
 
 int main() {
 
@@ -8,27 +11,41 @@ int main() {
 	char ask[] = "How can I help? (Enter help for suggestions.)\n";
 	char confused[] = "I don't understand.\n";
 	char joke[] = "My dog has no nose. It smells terribly!\n";
-	char helpList[] = "Try entering:\njoke\nexit\n";
+	char helpList[] = "Try entering:\njoke\nsay\nrps\nexit\n";
+	char say[] = "What would you like me to say?\n";
+	char chooserps[] = "Choose rock, paper or scissors.\n";
 
-	char input[100];
+	char userInput[100];
 
 
 	printf("%s", intro);
 
 	while (1) {
 		printf("%s", ask);
-		scanf("%s", input);
+		scanf("%s", userInput);
 
-		if ( 0 == (strcmp(input, "exit")) ) {
+		if ( 0 == (strcmp(userInput, "exit")) ) {
 			exit(0);
 		}
 
-		else if ( 0 == (strcmp(input, "help")) ) {
+		else if ( 0 == (strcmp(userInput, "help")) ) {
 			printf("%s", helpList);
 		}
 
-		else if ( 0 == (strcmp(input, "joke")) ) {
+		else if ( 0 == (strcmp(userInput, "joke")) ) {
 			printf("%s", joke);
+		}
+
+		else if ( 0 == (strcmp(userInput, "say")) ) {
+			printf("%s", say);
+			scanf("%s", userInput);
+			printf("%s\n", userInput);
+		}
+
+		else if ( 0 == (strcmp(userInput, "rps")) ) {
+			printf("%s", chooserps);
+			scanf("%s", userInput);
+			rps(userInput);
 		}
 
 		else {
@@ -38,4 +55,78 @@ int main() {
 	}
 
 	return 0;
+}
+
+void rps(char* userInput) {
+	int player1, player2, wld;
+	time_t t;
+	srand((unsigned) time(&t));
+
+
+
+	/* determine player1 choice */
+	if ( 0 == (strcmp(userInput, "rock")) ) {
+		player1 = 0;
+		printf("You chose %s.\n", userInput);
+	}
+
+	else if ( 0 == (strcmp(userInput, "paper")) ) {
+		player1 = 1;
+		printf("You chose %s.\n", userInput);
+	}
+
+	else if ( 0 == (strcmp(userInput, "scissors")) ) {
+		player1 = 2;
+		printf("You chose %s.\n", userInput);
+	}
+
+	else {
+		player1 = 3;
+		printf("You can't choose %s!\n", userInput);
+	}
+
+	/* randomnly generate rps */
+	player2 = rand() % 3;
+
+	if (player2 == 0) {
+		printf("I chose rock!\n");
+	}
+
+	else if (player2 == 1) {
+		printf("I chose paper!\n");
+	}
+
+	else if (player2 == 2) {
+		printf("I chose scissors!\n");
+	}
+
+	/* determine winner */
+	if (player1 == player2) {
+		printf("The game was a draw!\n");
+	}
+
+	else if ((player1 == 0) && (player2 == 1)) {
+		printf("I win!\n");
+	}
+
+	else if ((player1 == 0) && (player2 == 2)) {
+		printf("You win!\n");
+	}
+
+	else if ((player1 == 1) && (player2 == 0)) {
+		printf("You win!\n");
+	}
+
+	else if ((player1 == 1) && (player2 == 2)) {
+		printf("I win!\n");
+	}
+
+	else if ((player1 == 2) && (player2 == 0)) {
+		printf("I win!\n");
+	}
+
+	else if ((player1 == 2) && (player2 == 1)) {
+		printf("You win!\n");
+	}
+
 }
